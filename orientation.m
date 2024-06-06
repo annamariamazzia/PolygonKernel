99dff1b80eb12aec669d8968ff1926b9ec34a3a0
Annamaria Mazzia <annamaria.mazzia@unipd.it>
2024-06-06 12:50:16 +0200

    Update orientation.m

--- orientation.m
+++ orientation.m
@@ -4,9 +4,9 @@ xcoord=xcoord(:);
 ycoord=ycoord(:);
 orien=det([ones(3,1), xcoord, ycoord]);
 if orien>toll 
-    ori=-1;  % counter-clockwise
+    ori=1;  % counter-clockwise
 elseif orien<-toll
-    ori=1;  %clockwise
+    ori=-1;  %clockwise
 else
     ori=0;
 end
