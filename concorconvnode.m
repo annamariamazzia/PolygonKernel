cae667f2d9cd9dcd0b26e4b3a8abf36cb06b00db
Annamaria Mazzia <annamaria.mazzia@unipd.it>
2025-01-17 17:43:53 +0100

    Update concorconvnode.m

--- concorconvnode.m
+++ concorconvnode.m
@@ -6,13 +6,14 @@ function convconcvert=concorconvnode(nP,xvert,yvert,iv)
 %       iv  vertex
 % output convconcvert =1 if iv is a concave vertex
 %        convconvvert=0  if iv is a convex vertex
+     toll=1e-10;
     ivm1=mod(iv-2,nP)+1;
     ivp1=mod(iv,nP)+1;
     vetta=[xvert(ivp1)- xvert(iv),yvert(ivp1)- yvert(iv) ];
     vettb=[xvert(ivm1)- xvert(iv),yvert(ivm1)- yvert(iv) ];
     alpha = atan2((det([vetta;vettb])),dot(vetta,vettb)); 
     alpha = mod(alpha,2*pi);
-    if alpha>pi
+    if alpha-pi> toll 
         convconcvert=1; %concave vertex
     else
         convconcvert=0; %convex vertex
